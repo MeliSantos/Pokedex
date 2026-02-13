217dec49e98ae8b18f23cd5cb9f8cd4c0d6cd896
Ellen Santos <ellensantos10@hotmail.com>
2026-02-13 16:15:38 +0100

    Search function added, favicon added

--- script.js
+++ script.js
@@ -33,16 +33,22 @@ function init() {
 async function fetchPokemonList() {
   let response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=25&offset=0');
   let data = await response.json();
-  allPokemons = data.results;
-  data.results.forEach((pokemon, index) => {
-    fetchPokemonDetails(pokemon.url, index);
-  });
+
+  for (let i = 0; i < data.results.length; i++) {
+    let detailResponse = await fetch(data.results[i].url);
+    let pokemon = await detailResponse.json();
+    pokemon.index = i;
+    allPokemons.push(pokemon);
+    createPokemonCard(pokemon);
+  }
 }
+
 
 async function fetchPokemonDetails(url, index) {
   let response = await fetch(url);
   let pokemon = await response.json();
   pokemon.index = index;
+  allPokemons[index] = pokemon;
   createPokemonCard(pokemon);
 }
 
@@ -84,14 +90,12 @@ function openPokemonModal(index) {
   showPokemonModal(allPokemons[currentPokemonIndex]);
 }
 
-async function showPokemonModal(pokemonSummary) {
+async function showPokemonModal(pokemon) { // Parameter jetzt "pokemon"
   document.getElementById("modalLoading").classList.remove("d-none");
   document.getElementById("modalContent").classList.add("d-none");
   myModal.show();
-  let response = await fetch(pokemonSummary.url);
-  let pokemon = await response.json();
 
-
+  // Alles innerhalb der Funktion nutzt jetzt "pokemon" statt "pokemonSummary" oder undefiniert
   document.getElementById('pokemonModalLabel').textContent = pokemon.name;
   document.getElementById('modalImage').src = pokemon.sprites.front_default;
   let modalBgColor = typeColors[pokemon.types[0].type.name];
@@ -103,9 +107,6 @@ async function showPokemonModal(pokemonSummary) {
     typesHtml += `<span class="badge me-1" style="background-color:${typeColors[typeName]}; color:white; border-radius:12px; padding:4px 10px;">${typeName}</span>`;
   }
   document.getElementById('modalTypes').innerHTML = typesHtml;
-
-
-
 
   let statsHtml = "";
   for (let i = 0; i < pokemon.stats.length; i++) {
@@ -121,6 +122,7 @@ async function showPokemonModal(pokemonSummary) {
   }
 
   document.getElementById('modalStats').innerHTML = statsHtml;
+
   let heightInMeters = pokemon.height / 10;
   let weightInKg = pokemon.weight / 10;
   let mainInfoHtml = `
@@ -151,12 +153,12 @@ async function showPokemonModal(pokemonSummary) {
     </span>
   </div>
 `;
+
   document.getElementById("modalMainInfo").innerHTML = mainInfoHtml;
-
-
   document.getElementById("modalLoading").classList.add("d-none");
   document.getElementById("modalContent").classList.remove("d-none");
 }
+
 
 async function showNext(direction) {
   if (currentPokemonIndex === -1) return;
@@ -177,3 +179,49 @@ triggerTabList.forEach(triggerEl => {
     tabTrigger.show()
   })
 })
+
+
+function searchPokemon() {
+  let inputField = document.getElementById('searchInput');
+  let input = inputField.value.toLowerCase().trim();
+
+  if (input.length < 3) {
+    displayPokemonCards(allPokemons);
+    alert(' Please enter more than 2 letters to start the search.. ')
+    return;
+  }
+
+  let filteredPokemons = allPokemons.filter(pokemon =>
+    pokemon.name.toLowerCase().includes(input)
+  );
+
+  if (filteredPokemons.length === 0) {
+    showNoResults();
+  } else {
+    displayPokemonCards(filteredPokemons);
+  }
+    inputField.value = "";
+}
+
+
+
+function displayPokemonCards(pokemonList) {
+  let container = document.getElementById('poke-cards');
+  container.innerHTML = '';
+
+  pokemonList.forEach(pokemon => {
+    createPokemonCard(pokemon);
+  });
+}
+function showNoResults() {
+  let container = document.getElementById('poke-cards');
+  container.innerHTML = `
+    <div class="centerAlert">
+      <p class="alertNoPoke">
+        No Pokémon found... 😢
+      </p>
+    </div>
+  `;
+}
+
+
